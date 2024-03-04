1d2668b1ec0afad66e96cafa0ba5b20299d1277c
Lauren Yim <31467609+cherryblossom000@users.noreply.github.com>
2024-03-05 02:50:01 +1100

    explain frontend/src/main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,6 @@
+// This is only to demonstrate how to import from mac-jobs-shared if needed - pls get rid of it
 import {message} from 'mac-jobs-shared';
+// but keep this import - it lets Vite know to include the CSS file
 import './style.css';
 
 alert(message);
