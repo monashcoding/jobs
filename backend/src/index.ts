72a03546122e1f2c1ec95b72e0cf317a822e0a28
Lauren Yim <31467609+cherryblossom000@users.noreply.github.com>
2024-03-15 14:28:14 +1100

    initial /api/opportunities api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
+import path from 'node:path';
 import express from 'express';
+import apiRouter from './api.js';
 
 const app = express();
 
-app.use(
-	express.static(new URL('../../frontend/dist', import.meta.url).pathname),
-);
+app.use(express.static(path.join(import.meta.dirname, '../../frontend/dist')));
+
+app.use('/api', apiRouter);
 
 const port = Number(process.env.PORT ?? 3000);
 app.listen(port, () => console.log(`listening on http://localhost:${port}`));
--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,1 +1,66 @@
 export const message = 'mac job board 🔥';
+
+export const enum OpportunityType {
+	Program = 'Program',
+	WinterInternship = 'Winter Internship',
+	SummerInternship = 'Summer Internship',
+	SixTwelveMonthInternship = '6/12 Month Internship',
+	OneYearPlusInternship = '1 Year+ Internship',
+	GradProgram = 'Grad Program',
+}
+
+export const enum CourseProgression {
+	PrePenultimate = 'Pre-Penultimate',
+	Penultimate = 'Penultimate',
+	Grad = 'Ultimate/Grad',
+}
+
+export const enum OpportunityStatus {
+	Open = 'Open',
+	Closed = 'Closed',
+}
+
+export const enum WorkingRights {
+	AustralianCitizen = 'Australian Citizen',
+	NewZealandCitizen = 'New Zealand Citizen',
+	AustralianPermanentResident = 'Australian Permanent Resident',
+	FullWorkingRights = 'Full Working Rights',
+	International = 'International',
+	Subclass462 = 'Subclass 462',
+	AustralianTemporaryWorkVisa = 'Australian Temporary Work Visa',
+	NewZealandPermanentResident = 'New Zealand Permanent Resident',
+	AustralianStudentVisa = 'Australian Student Visa',
+}
+
+export const enum Month {
+	January = 'January',
+	February = 'February',
+	March = 'March',
+	April = 'April',
+	May = 'May',
+	June = 'June',
+	July = 'July',
+	August = 'August',
+	September = 'September',
+	October = 'October',
+	November = 'November',
+	December = 'December',
+}
+
+export interface Opportunity {
+	company: string;
+	name: string;
+	type: OpportunityType;
+	courseProgressions: CourseProgression[];
+	status: OpportunityStatus;
+	roles: string[];
+	locations: string[];
+	duration?: string | undefined;
+	openMonth?: Month | undefined;
+	closeMonth?: Month | undefined;
+	industries: string[];
+	workingRights: WorkingRights[];
+	url?: string | undefined;
+	description?: string | undefined;
+	notes?: string | undefined;
+}
