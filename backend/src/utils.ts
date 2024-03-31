57bc1db4ba3cbd1e439d5e579b87e3b8a31c7ac0
Lauren Yim <31467609+cherryblossom000@users.noreply.github.com>
2024-03-31 22:10:01 +1100

    backend: implement other filters for status/type/course progression/working rights/location/industries

--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -2,29 +2,7 @@ import type {NextFunction, Request, RequestHandler, Response} from 'express';
 import asyncHandler from 'express-async-handler';
 import type * as z from 'zod';
 
-export const parseQuery =
-	<T extends z.AnyZodObject>(
-		T: T,
-		handler: (
-			data: z.TypeOf<T>,
-			req: Request,
-			res: Response,
-			next: NextFunction,
-		) => void,
-	): RequestHandler =>
-	(req, res, next) => {
-		let data: z.TypeOf<T>;
-		try {
-			data = T.parse(req.query);
-		} catch (error: unknown) {
-			res.status(400).send(`Invalid query: ${String(error)}`);
-			return;
-		}
-
-		handler(data, req, res, next);
-	};
-
-export const parseQueryAsync = <T extends z.AnyZodObject>(
+export const parseQuery = <T extends z.AnyZodObject>(
 	T: T,
 	handler: (
 		data: z.TypeOf<T>,
