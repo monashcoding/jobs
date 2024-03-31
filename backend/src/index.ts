0c6a10a4a22d29bf9c465295c62a0cbf497e3d02
Lauren Yim <31467609+cherryblossom000@users.noreply.github.com>
2024-03-31 19:48:02 +1100

    fix paths for windows
    also don't use import.meta.dirname as it is a release candidate and not stable yet

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,14 @@
-import path from 'node:path';
+import {fileURLToPath} from 'node:url';
 import express from 'express';
 import apiRouter from './api.js';
 
 const app = express();
 
-app.use(express.static(path.join(import.meta.dirname, '../../frontend/dist')));
+app.use(
+	express.static(
+		fileURLToPath(new URL('../../frontend/dist/', import.meta.url)),
+	),
+);
 
 app.use('/api', apiRouter);
 
