2bab893b194c73c9f82b65f58534667cf8caf298
monash-coding <64454397+monash-coding@users.noreply.github.com>
2024-04-01 21:42:02 +1100

    Feature filter buttons (#1)
    * Initial implementation of filter buttons
    
    * Added click toggle functionality to all buttons
    
    * getSelectedFilters works now
    
    * Removed debug code
    
    ---------
    
    Co-authored-by: Christian <cmoo0017@student.monash.edu>

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,5 @@
 // This is only to demonstrate how to import from mac-jobs-shared if needed - pls get rid of it
-import {message} from 'mac-jobs-shared';
+import { message } from 'mac-jobs-shared';
 // but keep this import - it lets Vite know to include the CSS file
 import './style.css';
 
